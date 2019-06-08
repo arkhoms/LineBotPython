18581ed0cdfa00dfa5b16e1f94284da1c71f287a
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-08 22:41:27 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,5 +5,18 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+# ส่วน callback สำหรับ Webhook
+@app.route('/callback', methods=['POST'])
+def callback():
+  json_line = request.get_json()
+  json_line = json.dumps(json_line)
+  decoded = json.loads(json_line)
+  user = decoded["events"][0]['replyToken']
+  #id=[d['replyToken'] for d in user][0]
+  #print(json_line)
+  print("ผู้ใช้：",user)
+  sendText(user,'งง') # ส่งข้อความ งง
+  return '',200
+
 if __name__ == "__main__":
     app.run()
