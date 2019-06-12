995e2a1a761d6237485a0c422556c85467ab4045
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-06-12 23:39:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ['INE_CHANNEL_ACCESS_TOKEN'])
+line_bot_api = LineBotApi(os.environ['LINE_CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['LINE_CHANNEL_SECRET'])
 
 @app.route("/")
